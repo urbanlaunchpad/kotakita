4cbd249cb0b7567a6490fc97e542001d168710be
Stephen Kennedy <stephenjameskennedy@gmail.com>
2015-03-30 23:01:10 -0700

    Remove source files

--- en/src/js/toggle.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-$( "#language-toggle" ).click(function(){
-  $( ".text-on" ).switchClass( "text-on", "text-off", 10 );
-  $( ".text-off" ).switchClass( "text-off", "text-on", 10 );
-});
-});
